eef92931c41e0ada1cb3397379150dfaa2d1ba33
arkadye <suicidalduckling@gmail.com>
2025-04-19 01:46:34 +0100

    Working C interface and mono interface

--- source_code/clib/include/tournament_builder.h
+++ source_code/clib/include/tournament_builder.h
@@ -6,7 +6,7 @@
 */
 
 /*
-	To compile for DLL export, set define TOURNAMENT_BUILDER_ENABLE_EXPORT_TO_DLL to 1.
+	To compile for DLL export, set define TOURNAMENT_BUILDER_ENABLE_EXPORT_TO_DLL.
 */
 
 #ifdef TOURNAMENT_BUILDER_ENABLE_EXPORT_TO_DLL
@@ -15,26 +15,20 @@
 #define TOURNAMENT_BUILDER_API
 #endif
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-	/*
-		Params:
-			input: Either a JSON string or a filename containing JSON representing a World.
-			output_filename: The file to write the output to.
-	*/
-	TOURNAMENT_BUILDER_API void tournament_builder_make_tournament_to_file(const char* input, const char* output_filename);
 
-	/*
-		Params:
-			input: Either a JSON string or a filename containing JSON representing a World.
-		Returns:
-			A char* array containing a JSON object representing a World. This must have free() called on it by the user.
-	*/
-	TOURNAMENT_BUILDER_API char* tournament_builder_make_tournament(const char* input);
-#ifdef __cplusplus
-}
-#endif
+/*
+	Params:
+		input: Either a JSON string or a filename containing JSON representing a World.
+		output_filename: The file to write the output to.
+*/
+TOURNAMENT_BUILDER_API void tournament_builder_make_tournament_to_file(const char* input, const char* output_filename);
+
+/*
+	Params:
+		input: Either a JSON string or a filename containing JSON representing a World.
+	Returns:
+		A char* array containing a JSON object representing a World. This must have free() called on it by the user.
+*/
+TOURNAMENT_BUILDER_API char* tournament_builder_make_tournament(const char* input);
 
 #undef TOURNAMENT_BUILDER_EXPORT
--- source_code/mono/src/tournament_builder_mono.cs
+++ source_code/mono/src/tournament_builder_mono.cs
@@ -1,18 +1,18 @@
 using System;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace TournamentBuilder
 {
 	public static class TourmanentBuilder
 	{
-        [DllImport("tournament_builder_clib.lib", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("tournament_builder_clib", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
         static extern void tournament_builder_make_tournament_to_file(
             [MarshalAs(UnmanagedType.LPStr)] string input,
             [MarshalAs(UnmanagedType.LPStr)] string output_filename);
 
-        [DllImport("tournament_builder_clib.lib", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
-        [return: MarshalAs(UnmanagedType.BStr)]
-        static extern string tournament_builder_make_tournament(
+        [DllImport("tournament_builder_clib", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        static extern IntPtr tournament_builder_make_tournament(
             [MarshalAs(UnmanagedType.LPStr)] string input);
 
         /*
@@ -21,9 +21,11 @@ namespace TournamentBuilder
 		Returns:
 			A string containing a JSON object representing a World. This must have free() called on it by the user.
 	*/
-        public static string MakeTournament(string input)
+        public static string MakeTournament(ref string input)
         {
-            return tournament_builder_make_tournament(input);
+            IntPtr makeResult = tournament_builder_make_tournament(input);
+            string retResult = System.Runtime.InteropServices.Marshal.PtrToStringAnsi(makeResult);
+            return retResult;
         }
 
         /*
@@ -31,9 +33,9 @@ namespace TournamentBuilder
 			input: Either a JSON string or a filename containing JSON representing a World.
 			output_filename: The file to write the output to.
 	    */
-        public static void MakeTournamentToFile(string input, string output_filename)
+        public static void MakeTournamentToFile(ref string input, ref string output_filename)
         {
             tournament_builder_make_tournament_to_file(input, output_filename);
         }
-	}
+    }
 }
