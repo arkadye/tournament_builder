51687b48731e161e4657c2d93da5816ccf2b3360
arkadye <suicidalduckling@gmail.com>
2025-04-20 11:57:14 +0100

    Add helper bat file to create packages in the desired location

---
+++ make_release_packages.bat
@@ -0,0 +1,20 @@
+rem Build and make the release packages.
+rem Requires cmake to be installed.
+
+IF DEFINED CONFIG (
+	echo "Config set to '%CONFIG%'"
+) ELSE (
+	SET CONFIG="Release"
+)
+
+IF DEFINED DESTINATION (
+	echo "Destination set to '%DESTINATION%'"
+) ELSE (
+	SET DESTINATION="../packages"
+)
+
+PUSHD .
+
+cd /D %~dp0/source_code
+cmake . && cmake --build . --config %CONFIG% && cmake --install . --config Release --prefix %DESTINATION%/%CONFIG%
+POPD
