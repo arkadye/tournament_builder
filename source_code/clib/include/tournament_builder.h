bcad68ef3ae22cb20889d7d36a6e4bf67da06a5e
arkadye <suicidalduckling@gmail.com>
2025-05-05 22:07:23 +0100

    Setup cmake to generate a header for the C interface exports

--- source_code/clib/include/tournament_builder.h
+++ source_code/clib/include/tournament_builder.h
@@ -5,20 +5,12 @@
 	anyone writing C to use this directly, but LUA and Python bindings to C interfaces are easier with a C interface.
 */
 
-/*
-	To compile for DLL export, set define TOURNAMENT_BUILDER_ENABLE_EXPORT_TO_DLL.
-*/
+#include "tournament_builder/clib_export.h"
 
 #if __cplusplus
 #define EXTERN_C_DECL extern "C"
 #else
 #define EXTERN_C_DECL
-#endif
-
-#ifdef TOURNAMENT_BUILDER_ENABLE_EXPORT_TO_DLL
-#define TOURNAMENT_BUILDER_API __declspec(dllexport)
-#else
-#define TOURNAMENT_BUILDER_API
 #endif
 
 
@@ -27,7 +19,7 @@
 		input: Either a JSON string or a filename containing JSON representing a World.
 		output_filename: The file to write the output to.
 */
-EXTERN_C_DECL TOURNAMENT_BUILDER_API void tournament_builder_make_tournament_to_file(const char* input, const char* output_filename);
+EXTERN_C_DECL TOURNAMENT_BUILDER_CLIB_EXPORT void tournament_builder_make_tournament_to_file(const char* input, const char* output_filename);
 
 /*
 	Params:
@@ -35,7 +27,6 @@ EXTERN_C_DECL TOURNAMENT_BUILDER_API void tournament_builder_make_tournament_to_
 	Returns:
 		A char* array containing a JSON object representing a World. This must have free() called on it by the user.
 */
-EXTERN_C_DECL TOURNAMENT_BUILDER_API char* tournament_builder_make_tournament(const char* input);
+EXTERN_C_DECL TOURNAMENT_BUILDER_CLIB_EXPORT char* tournament_builder_make_tournament(const char* input);
 
-#undef TOURNAMENT_BUILDER_EXPORT
 #undef EXTERN_C_DECL
