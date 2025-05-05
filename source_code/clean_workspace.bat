0289a966c89b0f58ec72670e3b21e61c6d0cd24a
arkadye <suicidalduckling@gmail.com>
2025-05-05 22:08:01 +0100

    Centralise the clean workspace batch file to one place.

--- source_code/clean_workspace.bat
+++ source_code/clean_workspace.bat
@@ -1,10 +1,22 @@
+echo "CALLED clean_workspace.bat arg = [%1]"
+if [%1]==[] goto NOARG
+
+set TARGET=%1
+echo "Running clean on %TARGET%"
+
 pushd .
-cd @%~dp0
+cd %TARGET%
+
 del *.vcxproj
 del *.vcxproj.*
+del *.vsproj
+del *.vsproj.*
 del *.sln
 del cmake_install.cmake
 del CMakeCache.txt
+del *.cmake
+del *.tcl
+
 rmdir /S/Q .vs
 rmdir /S/Q x64
 rmdir /S/Q CMakeFiles
@@ -12,10 +24,29 @@ rmdir /S/Q Debug
 rmdir /S/Q Release
 rmdir /S/Q MinSizeRel
 rmdir /S/Q RelWithDebInfo
+rmdir /S/Q tournament_builder_clib.dir
+rmdir /S/Q tournament_builder_cpplib.dir
+rmdir /S/Q tournament_builder_cli.dir
+rmdir /S/Q tournament_builder_cli_lib.dir
+rmdir /S/Q tournament_builder_dbg.dir
 
-cli/clean_workspace.bat
-cli_dbg/clean_workspace.bat
-clib/clean_workspace.bat
-cpplib/clean_workspace.bat
-mono/clean_workspace.bat
 popd
+goto FINISH
+
+:NOARG
+echo "No argument found. Using local directory (%~dp0) and running resursively."
+pushd .
+cd %~dp0
+call clean_workspace.bat %~dp0
+call clean_workspace.bat %~dp0/cli
+call clean_workspace.bat %~dp0/cli_dbg
+call clean_workspace.bat %~dp0/clib
+call clean_workspace.bat %~dp0/cpplib
+call clean_workspace.bat %~dp0/mono
+popd 
+goto FINISH
+
+
+
+:FINISH
+echo "FINISHED clean_workspace.bat arg = [%1]"
