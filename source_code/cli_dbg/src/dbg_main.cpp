e9ddcbf193cf2593f788f88a990c561e234ca58a
arkadye <suicidalduckling@gmail.com>
2025-05-13 14:43:10 +0100

    Update dbg mode to support example cases in subfolders.

--- source_code/cli_dbg/src/dbg_main.cpp
+++ source_code/cli_dbg/src/dbg_main.cpp
@@ -8,9 +8,14 @@
 #include <iostream>
 #include <format>
 
+
 #ifndef EXAMPLES_FOLDER_DEFAULT_LOCATION
 #define EXAMPLES_FOLDER_DEFAULT_LOCATION "../../examples"
 #endif
+
+std::vector<char> make_c_str_vec(const auto& stringish);
+std::pair<int, int> run_testcases_on_examples(auto& args_array, const std::filesystem::path& input_directory, const std::filesystem::path& output_directory);
+
 
 // Set this to only run on files containing this pattern.
 constexpr std::string_view PATTERN_TO_MATCH{ "json" };
@@ -20,9 +25,8 @@ constexpr bool testing = true;
 
 int main(int argc, char** argv)
 {
-	using tournament_builder::tournament_builder_internal::invoke_cli;
-
 	std::filesystem::path target{ EXAMPLES_FOLDER_DEFAULT_LOCATION };
+
 	if (argc > 1)
 	{
 		target = argv[1];
@@ -42,15 +46,6 @@ int main(int argc, char** argv)
 
 	constexpr int num_args = 7;
 	std::array<char*, num_args> args{};
-
-	auto make_c_str_vec = [](const auto& stringish) -> std::vector<char>
-		{
-			std::vector<char> result;
-			result.reserve(std::size(stringish) + 1);
-			std::ranges::copy(stringish, std::back_inserter(result));
-			result.push_back('\0');
-			return result;
-		};
 
 	auto input_arg = make_c_str_vec("--input");
 	auto output_arg = testing ? make_c_str_vec("--test-reference") : make_c_str_vec("--output");
@@ -82,34 +77,61 @@ int main(int argc, char** argv)
 	const std::filesystem::path output_directory = target / "out";
 	std::filesystem::create_directories(output_directory);
 
+	const auto [successes, failures] = run_testcases_on_examples(args, input_directory, output_directory);
+	std::cout << std::format("Finished with {} successes and {} failures.\n", successes, failures);
+	return 0;
+}
+
+std::vector<char> make_c_str_vec(const auto& stringish)
+{
+	std::vector<char> result;
+	result.reserve(std::size(stringish) + 1);
+	std::ranges::copy(stringish, std::back_inserter(result));
+	result.push_back('\0');
+	return result;
+};
+
+// Returns successes / failures
+std::pair<int, int> run_testcases_on_examples(auto& args, const std::filesystem::path& input_directory, const std::filesystem::path& output_directory)
+{
+	using tournament_builder::tournament_builder_internal::invoke_cli;
 	int successes = 0;
 	int failures = 0;
-
 
 	for (std::filesystem::path input_file : std::filesystem::directory_iterator{ input_directory })
 	{
 		std::filesystem::path filename = input_file.filename();
+		if (std::filesystem::is_directory(input_file))
 		{
-			const std::string filename_str = filename.generic_string();
-			const std::string_view filename_sv{ filename_str };
-			const std::size_t find_result = filename_sv.find(PATTERN_TO_MATCH);
-			const bool found = find_result < filename_sv.size();
-			if (!found)
-			{
-				std::cout << std::format("Skipping file '{}' - did not match pattern '{}'\n", filename_sv, PATTERN_TO_MATCH);
-				continue;
-			}
+			const std::filesystem::path in = input_directory / filename;
+			const std::filesystem::path out = output_directory / filename;
+			auto [subsuccesses, subfailures] = run_testcases_on_examples(args, in, out);
+			successes += subsuccesses;
+			failures += subfailures;
 		}
-		std::filesystem::path output_file = { target / "out" / filename };
-		
-		auto infile_data = make_c_str_vec(input_file.generic_string());
-		auto outfile_data = make_c_str_vec(output_file.generic_string());
-		args[2] = infile_data.data();
-		args[4] = outfile_data.data();
-		std::cout << std::format("Processing {} and {} {}.\n", filename.generic_string(), testing ? "checking the result against" : "writing the result to", std::filesystem::absolute(output_file).generic_string());
-		const bool success = (EXIT_SUCCESS == invoke_cli(num_args, args.data()));
-		success ? ++successes : ++failures;
+		else
+		{
+			{
+				const std::string filename_str = filename.generic_string();
+				const std::string_view filename_sv{ filename_str };
+				const std::size_t find_result = filename_sv.find(PATTERN_TO_MATCH);
+				const bool found = find_result < filename_sv.size();
+				if (!found)
+				{
+					std::cout << std::format("Skipping file '{}' - did not match pattern '{}'\n", filename_sv, PATTERN_TO_MATCH);
+					continue;
+				}
+			}
+			std::filesystem::path output_file = { output_directory / filename };
+
+			auto infile_data = make_c_str_vec(input_file.generic_string());
+			auto outfile_data = make_c_str_vec(output_file.generic_string());
+			args[2] = infile_data.data();
+			args[4] = outfile_data.data();
+			std::cout << std::format("Processing {} and {} {}.\n", filename.generic_string(), testing ? "checking the result against" : "writing the result to", std::filesystem::absolute(output_file).generic_string());
+			const bool success = (EXIT_SUCCESS == invoke_cli(static_cast<int>(args.size()), args.data()));
+			success ? ++successes : ++failures;
+		}
 	}
-	std::cout << std::format("Finished with {} successes and {} failures.\n", successes, failures);
-	return 0;
+	return { successes , failures };
 }
