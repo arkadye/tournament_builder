1e1516c9c81d5cba89a6fe71f73cd3e834b26727
arkadye <suicidalduckling@gmail.com>
2026-01-25 17:09:55 +0000

    Add a stub class for holding extra args

---
+++ source_code/cpplib/include/tournament_builder/extra_args.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace tournament_builder
+{
+	// A way to pass in extra arguments. Typically these will be command line
+	// arguments that modify behaviour.
+	class ExtraArgs
+	{
+
+	};
+}
