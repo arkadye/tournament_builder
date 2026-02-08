c3aa7a2997f586af84afcc6a1c87883f96490f5e
arkadye <suicidalduckling@gmail.com>
2026-02-08 23:07:25 +0000

    Add more files to clean to clean_workspace

--- source_code/clean_workspace.bat
+++ source_code/clean_workspace.bat
@@ -1,3 +1,4 @@
+setlocal
 echo "CALLED clean_workspace.bat arg = [%1]"
 if [%1]==[] goto NOARG
 
@@ -26,11 +27,21 @@ rmdir /S/Q Release
 rmdir /S/Q MinSizeRel
 rmdir /S/Q Testing
 rmdir /S/Q RelWithDebInfo
+rmdir /S/Q ALL_BUILD.dir
+rmdir /S/Q Continuous.dir
+rmdir /S/Q Experimental.dir
+rmdir /S/Q INSTALL.dir
+rmdir /S/Q Nightly.dir
+rmdir /S/Q NightlyMemoryCheck.dir
+rmdir /S/Q RUN_TESTS.dir
+rmdir /S/Q ZERO_CHECK.dir
+rmdir /S/Q document_builder.dir
 rmdir /S/Q tournament_builder_clib.dir
 rmdir /S/Q tournament_builder_cpplib.dir
 rmdir /S/Q tournament_builder_cli.dir
 rmdir /S/Q tournament_builder_cli_lib.dir
 rmdir /S/Q tournament_builder_dbg.dir
+
 
 popd
 goto FINISH
@@ -44,7 +55,10 @@ call clean_workspace.bat %~dp0/cli
 call clean_workspace.bat %~dp0/cli_dbg
 call clean_workspace.bat %~dp0/clib
 call clean_workspace.bat %~dp0/cpplib
+call clean_workspace.bat %~dp0/cpplib
+call clean_workspace.bat %~dp0/documentation_builder
 call clean_workspace.bat %~dp0/mono
+call clean_workspace.bat %~dp0/scripts
 popd 
 goto FINISH
 
@@ -52,3 +66,4 @@ goto FINISH
 
 :FINISH
 echo "FINISHED clean_workspace.bat arg = [%1]"
+endlocal
